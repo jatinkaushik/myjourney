2377b7e03dcddbd72f73b6eba009e35a0640f6eb
Jatin Kaushik <kaushik.jatin16@gmail.com>
2025-05-21 04:46:37 +0530

    fix: adjusted position for duration and location in ExperienceCard component

--- src/app/components/design/experience.tsx
+++ src/app/components/design/experience.tsx
@@ -13,7 +13,7 @@ interface ExperienceCardProps {
 const ExperienceCard = (exp:ExperienceCardProps) => {
     return (
         <div className="flex flex-col lg:flex-row w-full shadow-lg rounded-3xl gap-2 items-top bg-background px-5 py-2 lg:px-10 lg:py-5">
-            <div className="text-xl order-1 lg:order-1 font-semibold text-primary-foreground items-start justify-start">
+            <div className="order-1 lg:order-1 font-semibold text-primary-foreground items-start justify-start">
                 <div className="flex relative w-32 h-8 mb-2">
                     <Image
                         src={exp.logo}
@@ -23,8 +23,16 @@ const ExperienceCard = (exp:ExperienceCardProps) => {
                         priority
                     />
                 </div>
+                <div className="flex flex-col item-start justify-start text-sm font-normal text-primary-foreground">
+                    <div>
+                        {exp.duration}
+                    </div>
+                    <div>
+                        {exp.location}
+                    </div>
+                </div>
             </div>
-            <div className="flex flex-1 flex-col order-3 lg:order-2 mb-4 text-sm lg:text-lg text-muted-foreground min-h-30 justify-center px-0 lg:px-20">
+            <div className="flex flex-1 flex-col order-2 lg:order-2 mb-4 text-sm lg:text-lg text-muted-foreground min-h-30 justify-center px-0 lg:px-20">
                 <div className="text-sm lg:text-lg font-semibold text-primary-foreground">
                     {exp.position}
                 </div>
@@ -39,14 +47,6 @@ const ExperienceCard = (exp:ExperienceCardProps) => {
                             ))
                         }
                     </ul>
-                </div>
-            </div>
-            <div className="flex flex-col order-2 lg:order-3 item-start lg:items-end justify-start text-primary-foreground">
-                <div>
-                    {exp.duration}
-                </div>
-                <div>
-                    {exp.location}
                 </div>
             </div>
         </div>
