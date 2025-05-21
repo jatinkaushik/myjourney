0538ff6406187ef1952f6652f65b84d715272cbe
Jatin Kaushik <kaushik.jatin16@gmail.com>
2025-05-21 23:01:40 +0530

    feat: add Google Analytics scripts to RootLayout for tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { ReduxProvider } from '@/app/components/redux/providers';
 import ThemeButton from "@/app/components/theme/ThemeButton";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,6 +17,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="p-[3%] pt-[15%] md:p-[8%]">
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-BSS74VXP39"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BSS74VXP39');
+          `}
+        </Script>
         <ReduxProvider>
           {children}
           <ThemeButton />
